fc16fb0f7f1e3579211d15b4c9b8ced1d3ddfcdc
armanfar <Ramin.Armanfar@msg.group>
2022-08-10 16:25:53 +0200

    firefox added.

--- bot1.py
+++ bot1.py
@@ -1,12 +1,23 @@
 # selenium 4
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.common.by import By
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
 
-browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
 
 # Open the Website
-# browser.get('https://ir-appointment.visametric.com/ir')
-# browser.get('https://www.python.org/')
-# print(browser.title)
-browser.quit()
+driver.get('https://ir-appointment.visametric.com/ir')
+print('>>>>>>>>>>>>>>>', driver.title)
+
+checkBox = driver.find_element(By.CLASS_NAME, "recaptcha-checkbox-checkmark")
+checkBox.click()
+
+schengenBtn = driver.find_element(By.ID, "schengenBtn")
+schengenBtn.click()
+
+
+time.sleep(5)
+driver.quit()
